ace3724da24b270e5ba723564c316902141c71a6
Justin Grote <jgrote@allieddigital.net>
2020-11-03 17:05:23 -0800

    Merge branch 'PWA'

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,20 +1,23 @@
 using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace BlazorApp.Client
+namespace Client
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
-            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
---
+++ Shared/PriceResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.Json.Serialization;
+// Generated from https://app.quicktype.io?share=BIWN5YNMt1MFeiqHapU1 and modified for System.Text.Json
+namespace Client.Shared
+{
+    public class PriceResponse
+    {
+        public string BillingCurrency { get; set; }
+        public string CustomerEntityId { get; set; }
+        public string CustomerEntityType { get; set; }
+        public Uri NextPageLink { get; set; }
+        public int Count { get; set; }
+        
+        [JsonPropertyName("Items")]
+        public Price[] Prices { get; set; }
+    }
+
+    public class Price
+    {
+        public string CurrencyCode { get; set; }
+        public double TierMinimumUnits { get; set; }
+        public double RetailPrice { get; set; }
+        public double UnitPrice { get; set; }
+        public string ArmRegionName { get; set; }
+        public string Location { get; set; }
+        public DateTimeOffset EffectiveStartDate { get; set; }
+        public Guid MeterId { get; set; }
+        public string MeterName { get; set; }
+        public string ProductId { get; set; }
+        public string SkuId { get; set; }
+        public string ProductName { get; set; }
+        public string SkuName { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceId { get; set; }
+        public string ServiceFamily { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public string Type { get; set; }
+        public bool IsPrimaryMeterRegion { get; set; }
+        public string ArmSkuName { get; set; }
+        public string ReservationTerm { get; set; }
+    }
+}
